9bc5c52a69ee5b6373584fa39912da257d0168ed
Ayush <thisisayu0912@gmail.com>
2025-12-25 22:11:21 +0530

    commit hierarchy implemented

--- ugit/base.py
+++ ugit/base.py
@@ -42,8 +42,39 @@ def get_tree(oid, base_path = ''):
             assert False, f'Unknown tree entry type: {type_}'
     return result
 
+def _empty_current_directory():
+    for root, dirnames, filenames in os.walk('.', topdown=False):
+        for filename in filenames:
+            if is_ignored(os.path.join(root, filename)):
+                continue
+            os.remove(os.path.join(root, filename))
+        for dirname in dirnames:
+            if is_ignored(os.path.join(root, dirname)):
+                continue
+            try:
+                os.rmdir(os.path.join(root, dirname))
+            except OSError:
+                pass
+
 def read_tree(tree_oid):
+    _empty_current_directory()
     for path, oid in get_tree(tree_oid, base_path='./').items():
         os.makedirs(os.path.dirname(path), exist_ok=True)
         with open(path, 'wb') as f:
             f.write(data.get_object(oid, 'blob'))
+
+def commit(message):
+    commit = f'tree {write_tree()}\n'
+    commit_data = f'tree {write_tree()}\n'
+    HEAD = data.get_HEAD()
+    if HEAD:
+        commit += f'parent {HEAD}\n'
+    commit += '\n'
+    commit += f'{message}\n'
+    oid = data.hash_object(commit.encode(), type_ = 'commit')
+        commit_data += f'parent {HEAD}\n'
+    commit_data += '\n'
+    commit_data += f'{message}\n'
+    oid = data.hash_object(commit_data.encode(), type_ = 'commit')
+    data.set_head(oid)
+    return oid
